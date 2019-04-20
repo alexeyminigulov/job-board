1848a47d9eae2b8c8192b2bc9bc14610e6604a5e
Alexey Minigulov <leha.minigulov@gmail.com>
2019-04-20 10:10:02 +0400

    Added layout for twig

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # job-board
 The simple website to search for job.
+
+To generation js and css use command:
+
+```sh
+gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+gulp.task('sass', function() {
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'assets/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("web/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('js', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("web/js"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', gulp.series('sass'), function() {
+
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'assets/scss/*.scss'], ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.parallel('js','serve'));
