36344f0b5088f76147182eca7a10d6f7f2320550
Alexey Minigulov <leha.minigulov@gmail.com>
2019-04-21 21:33:12 +0400

    Added admin template

---
+++ src/AppBundle/Controller/Admin/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/admin", name="admin_homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('admin/default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
