1bcd3eb6f58c959875fa2345dcc145fb8cc35818
Alexey Minigulov <leha.minigulov@mail.ru>
2019-04-23 23:24:33 +0400

    Added breadcrumbs

--- src/AppBundle/Controller/Admin/CompanyController.php
+++ src/AppBundle/Controller/Admin/CompanyController.php
@@ -5,16 +5,28 @@ namespace AppBundle\Controller\Admin;
 use AppBundle\Entity\Company;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use WhiteOctober\BreadcrumbsBundle\Model\Breadcrumbs;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CompanyController extends Controller
 {
+    private $breadcrumbs;
+
+    public function __construct(Breadcrumbs $breadcrumbs)
+    {
+        $this->breadcrumbs = $breadcrumbs;
+    }
+
     /**
      * @Route("/admin/companies", name="admin_companies")
      */
     public function indexAction()
     {
+        $this->breadcrumbs
+            ->addItem('Home', $this->get('router')->generate('admin_homepage'))
+            ->addItem('Companies');
+
         $companies = $this->getDoctrine()
             ->getRepository(Company::class)
             ->findAll();
@@ -30,6 +42,11 @@ class CompanyController extends Controller
      */
     public function showAction(Company $company)
     {
+        $this->breadcrumbs
+            ->addItem('Home', $this->get('router')->generate('admin_homepage'))
+            ->addItem('Companies', $this->get('router')->generate('admin_companies'))
+            ->addItem($company->getName());
+
         return $this->render('admin/company/show.html.twig', [
             'company' => $company,
         ]);
