f0698abb4de57bdd770f033e74beb6c73b2f4247
Alexey Minigulov <leha.minigulov@mail.ru>
2019-04-23 14:11:13 +0400

    Added user show

--- src/AppBundle/Controller/Admin/UserController.php
+++ src/AppBundle/Controller/Admin/UserController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller\Admin;
 
 use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,6 +12,7 @@ class UserController extends Controller
 {
     /**
      * @Route("/admin/users", name="admin_users")
+     * @Method("GET")
      */
     public function indexAction()
     {
@@ -20,6 +22,17 @@ class UserController extends Controller
 
         return $this->render('admin/user/index.html.twig', [
             'users' => $users,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/users/{id}", name="admin_users_show")
+     * @Method("GET")
+     */
+    public function showAction(User $user)
+    {
+        return $this->render('admin/user/show.html.twig', [
+            'user' => $user,
         ]);
     }
 }
