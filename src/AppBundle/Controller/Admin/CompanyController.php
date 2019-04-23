43ce2f4f8191d75ac836bdb40d19beae81fdfb44
Alexey Minigulov <leha.minigulov@gmail.com>
2019-04-23 11:22:02 +0400

    Added company show

--- src/AppBundle/Controller/Admin/CompanyController.php
+++ src/AppBundle/Controller/Admin/CompanyController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller\Admin;
 
 use AppBundle\Entity\Company;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,6 +21,17 @@ class CompanyController extends Controller
 
         return $this->render('admin/company/index.html.twig', [
             'companies' => $companies,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/companies/{id}", name="admin_companies_show")
+     * @Method("GET")
+     */
+    public function showAction(Company $company)
+    {
+        return $this->render('admin/company/show.html.twig', [
+            'company' => $company,
         ]);
     }
 }
